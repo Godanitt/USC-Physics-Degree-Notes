48dfd7143c1806df3a3a45e1e197c2e73595ac3a
Daniel Vázquez Lago <danielvazquezlago@gmail.com>
2024-09-23 12:18:00 +0200

    23/09 Apuntes simuulacion + particulas

---
+++ Notes/4-Fisica/Simulacion/Fotran/ej_1/Mod_01_Def_prec.f95
@@ -0,0 +1,9 @@
+module Mod_01_Def_prec
+    
+    	implicit none
+
+      integer, parameter :: entero=SELECTED_INT_KIND(9)
+      integer, parameter :: doblep=SELECTED_REAL_KIND(15,307)
+
+end module Mod_01_Def_prec
+    
---
+++ Notes/4-Fisica/Simulacion/Fotran/ej_1/Progm_01_Colocacion_inicial.f95
@@ -0,0 +1,102 @@
+! #####################################################################################################
+!   DEFINIMOS LAS VARIABLES
+!
+!       N -> número de partículas, que como debe verificar N=4M**3 -> N=4,32,108,256,500,864
+!       M -> dado un N, se define como M=(N/4)**(1/3)       
+!       rho -> densidad 
+!       V -> volumen de la caja
+!       L -> lado de la caja
+!       a -> mitad del lado de la caja, usado para colocar las partículas correctamente en la caja (fcc)
+!
+! #####################################################################################################
+!
+!   PRIMERA PARTE DEL PROGRAMA
+!
+!   En la primera parte asignamos a cada partícula una posición en los vectores rx,ry,rz
+!
+! #####################################################################################################
+!
+!   SEGUNDA PARTE DEL PROGRAMA
+!
+!   En la segunda parte del programa calculamos la energía potencial total del sistema
+!
+! #####################################################################################################
+
+
+PROGRAM Progm_01_Colocacion_inicial
+
+! IMPORTAMOS LOS MODULOS      
+
+      use Mod_01_Def_prec
+      !use variables_comunes
+
+! HACEMOS IMPLICITAS LAS VARIABLES
+      
+      implicit none
+      
+! DEFINIMOS LAS VARIABLES 
+
+      integer :: x,y,z,N,K,cuenta
+      real(kind=doblep) :: rho,V,L,a
+      real, allocatable :: rx(:),ry(:),rz(:)
+      
+! ASIGNAMOS VALORES A LAS VARIABLES
+
+
+      K=5
+      N=4*K*K*K   
+      rho = 0.5
+      L=10
+      a=L/2
+      V=L*L*L
+
+      cuenta = 0  
+      allocate(rx(N)); allocate(ry(N)); allocate(rz(N)) 
+      
+
+! PARTE 1: ASIGNAMOS POSICIONES INICIALES A LAS PARTICULAS 
+
+      DO x=0,K-1
+          DO y=0,K-1
+              DO z=0,K-1
+                
+                  rx(cuenta)=x*L
+                  ry(cuenta)=y*L
+                  rz(cuenta)=z*L
+                  
+                  rx(cuenta+1)=a+x*L
+                  ry(cuenta+1)=a+y*L
+                  rz(cuenta+1)=z*L
+                  
+                  rx(cuenta+2)=a+x*L
+                  ry(cuenta+2)=y*L
+                  rz(cuenta+2)=a+z*L
+
+                  rx(cuenta+3)=x*L
+                  ry(cuenta+3)=a+y*L
+                  rz(cuenta+3)=a+z*L
+                  
+                  cuenta = cuenta + 4
+                  
+                  PRINT*,cuenta,'z',z
+              ENDDO
+          ENDDO
+      ENDDO            
+
+
+! PARTE 2: CALCULAMOS LAS ENERGÍAS PONTENCIALES
+
+
+      DO x=0,500
+          DO y=0,500
+              DO z=0,500
+                  
+                  
+              ENDDO
+          ENDDO
+      ENDDO          
+
+
+      Pause 
+
+ENDPROGRAM Progm_01_Colocacion_inicial
