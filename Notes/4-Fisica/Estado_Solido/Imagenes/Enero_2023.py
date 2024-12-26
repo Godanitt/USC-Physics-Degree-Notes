3b17c08a41e9d9730fc789424c0b98a8b8baf409
Daniel Vázquez Lago <danielvazquezlago@gmail.com>
2024-12-26 18:49:53 +0200

    26/12/24
    Estado solido

--- Notes/4-Fisica/Estado_Solido/Imagenes/Enero_2023.py
+++ Notes/4-Fisica/Estado_Solido/Imagenes/Enero_2023.py
@@ -7,6 +7,7 @@ Created on Tue Dec 24 18:26:20 2024
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from lattpy import Lattice
 #import plotly.graph_objects as go
 
 
@@ -78,6 +79,7 @@ azul_clarito = (0.85, 0.85, 1)  # Equivalente al color definido en LaTeX
 fig.patch.set_facecolor(azul_clarito)  # Fondo de la figura
 ax.set_facecolor(azul_clarito)        # Fondo de los ejes
 
+verdeclarito=(0.66, 0.90, 0.56) #Verde clarito
 
 
 # Dibujamos las líneas
@@ -143,10 +145,127 @@ ax.set_zlim(0, n_cells * a)
 
 ax.set_xlim(0,1)
 ax.set_ylim(0,1)
-ax.set_zlim(0,2)
+ax.set_zlim(0,1)
 
 ax.view_init(elev=6.5, azim=12)
 
 
 plt.savefig("2023-Enero-01.pdf",dpi=300.0,bbox_inches="tight")
 plt.show()
+
+#%%# Función para dibujar la celda hexagonal
+def generate_hc_lattice(a, n_cells):
+    """
+    Genera una red cúbica centrada en las caras (FCC).
+    Args:
+        a: Parámetro de red (distancia entre átomos).
+        n_cells: Número de celdas unitarias en cada dirección.
+    Returns:
+        coords: Coordenadas de los átomos en la red FCC.
+    """
+    # Posiciones relativas dentro de una celda FCC
+    hc_basis = np.array([
+        [0.5, 0.5, 0.0],
+        
+        [0.5*(1+np.cos(np.pi/3)), 0.5*(1+np.sin(np.pi/3)), 0.0],
+        [1, 0.5, 0],
+        [0.5*(1+np.cos(np.pi/3)), 0.5*(1-np.sin(np.pi/3)), 0.0],
+        [0.5*(1-np.cos(np.pi/3)), 0.5*(1+np.sin(np.pi/3)), 0.0],
+        [0.0, 0.5, 0.0],
+        [0.5*(1-np.cos(np.pi/3)), 0.5*(1-np.sin(np.pi/3)), 0.0]
+        
+    ])
+    
+    coords = []
+    for k in [0,1]:
+        for basis in hc_basis:
+            if not(k+basis[2]>1.0):
+                coords.append((np.array([0, 0, k]) + basis) * a)
+    return np.array(coords)
+
+    
+    
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection='3d')
+azul_clarito = (0.85, 0.85, 1)  # Equivalente al color definido en LaTeX
+fig.patch.set_facecolor(azul_clarito)  # Fondo de la figura
+ax.set_facecolor(azul_clarito)        # Fondo de los ejes
+
+
+
+hc_coords= generate_hc_lattice(a, n_cells)
+
+ax.scatter(hc_coords[:, 0], hc_coords[:, 1], hc_coords[:, 2], c='blue',alpha=1.0, s=1000)
+for i in range(len(hc_coords)):
+    if (i<7):
+        ax.plot([hc_coords[i,0],hc_coords[0,0]],
+            [hc_coords[i,1],hc_coords[0,1]],
+            [hc_coords[i,2],hc_coords[0,2]],
+            color="black",
+            linewidth=2.0)
+        
+    if (i<6):
+        ax.plot([hc_coords[i,0],hc_coords[i+1,0]],
+            [hc_coords[i,1],hc_coords[i+1,1]],
+            [hc_coords[i,2],hc_coords[i+1,2]],
+            color="black",
+            linewidth=2.0)
+        
+        ax.plot([hc_coords[i,0],hc_coords[i+7,0]],
+            [hc_coords[i,1],hc_coords[i+7,1]],
+            [hc_coords[i,2],hc_coords[i+7,2]],
+            color="black",
+            linewidth=2.0)
+        
+
+        ax.plot([0.75,0.25], [0.0669873,0.0669873],[0,0],
+            color="black",
+            linewidth=2.0)
+        
+        ax.plot([0.75,0.25], [0.9330127,0.9330127],[0,0],
+            color="black",
+            linewidth=2.0)
+        
+        ax.plot([hc_coords[6,0],hc_coords[6+7,0]],
+            [hc_coords[6,1],hc_coords[6+7,1]],
+            [hc_coords[6,2],hc_coords[6+7,2]],
+            color="black",
+            linewidth=2.0)
+
+    if (i>6):
+        ax.plot([hc_coords[i,0],hc_coords[7,0]],
+            [hc_coords[i,1],hc_coords[7,1]],
+            [hc_coords[i,2],hc_coords[7,2]],
+            color="black",
+            linewidth=2.0)
+      
+    if (i<13 and i>6):
+        ax.plot([hc_coords[i,0],hc_coords[i+1,0]],
+            [hc_coords[i,1],hc_coords[i+1,1]],
+            [hc_coords[i,2],hc_coords[i+1,2]],
+            color="black",
+            linewidth=2.0)
+          
+
+        ax.plot([0.75,0.25], [0.0669873,0.0669873],[1,1],
+            color="black",
+            linewidth=2.0)
+        
+        ax.plot([0.75,0.25], [0.9330127,0.9330127],[1,1],
+            color="k",
+            linewidth=2.0)
+        
+fig.patch.set_facecolor(verdeclarito)  # Fondo de la figura
+ax.set_facecolor(verdeclarito) 
+ax.plot([1,0.5], [0.5,0.5],[0,0], color="red",  linewidth=2.0)
+ax.plot([1,1], [0.5,0.5],[0,1], color="red",  linewidth=2.0)
+ax.plot([1,0.75], [0.5,0.9330127],[0,0], color="red",  linewidth=2.0)
+        
+ax.view_init(elev=20, azim=22)
+
+# Opciones de gráfica
+ax.set_xlabel('X (Å)')
+ax.set_ylabel('Y (Å)')
+#ax.set_zlabel('Z (Å)')
+plt.savefig("2023-Enero-03.pdf",dpi=300.0,bbox_inches="tight")
