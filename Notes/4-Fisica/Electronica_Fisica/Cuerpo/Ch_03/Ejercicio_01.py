dda9728cffc4906264e0d92582235fb47cdf0f28
Godanitt <danielvazquezlago@gmail.com>
2025-03-08 15:10:00 +0100

    08/05/25 Electroncica + Lab SOlido

--- Notes/4-Fisica/Electronica_Fisica/Cuerpo/Ch_03/Ejercicio_01.py
+++ Notes/4-Fisica/Electronica_Fisica/Cuerpo/Ch_03/Ejercicio_01.py
@@ -1,4 +1,15 @@
+import sys
+import os
+
+# Get the absolute path of the parent directory
+module_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+
+# Add the folder2 path to sys.path
+sys.path.append(module_dir)
+
+# Now you can import script2.py
 from Ejercicios_modulo import *
+
 
 ##################################
 # Ejercicio 1 ####################
@@ -15,3 +26,45 @@ dP=0.008 # [cm]
 dN=0.008 # [cm]
 A=10**(-2) # [cm2]
 
+mnSi=1.18 # Masa del hueco en el silicio 300K) en m_e
+mpSi= 0.81 
+
+mn=fun_mnp(taun,mun)/me
+mp=fun_mnp(taup,mup)/me
+NC=fun_NCV(mnSi*me)
+NV=fun_NCV(mpSi*me)
+ni=fun_ni(NC,NV,EgSi,300)
+Vbi=fun_Vbi(NA,ND,ni)
+xn=fun_xn(NA,ND,KSSi,Vbi)
+xp=fun_xp(NA,ND,KSSi,Vbi)
+slopen=fun_slope_lineal(0,ND,KSSi)
+slopep=fun_slope_lineal(NA,0,KSSi)
+
+Ei=fun_Ei(ni,NA,ni)
+Ec=fun_Ec(mnSi*me,Ei,ni)
+Ev=fun_Ev(Ec)
+
+print("Apartado a)")
+print("mp_eff=%.5f me"%mp)
+print("mp_eff=%.5f me"%mn)
+print("NV=%.5e [cm-3]"%NV)
+print("NC=%.5e [cm-3]"%NC)
+print("ni=%.5e [cm-3]"%ni)
+print("pn=%.5e [cm^3]"%(ni**2/ND))
+print("Vbi=%.5e [V]"%Vbi)
+
+print("xp=%.5e [cm]"%xp)
+print("xn=%.5e [cm]"%xn)
+
+print("Ei=%.2f"%Ei)
+print("Ec=%.2f"%Ec)
+print("Ev=%.2f"%Ev)
+
+print("slopenp=",slopep)
+print("slopenn=",slopen)
+
+fig1=fun_grafica_bandas_pn(Ec,Ev,Vbi,Ei,slopep,slopen,xn,xp)
+
+fig1.savefig("03_01_Bandas.pdf")
+
+print("################")
--- Notes/4-Fisica/Electronica_Fisica/Cuerpo/Ejercicios_modulo.py
+++ Notes/4-Fisica/Electronica_Fisica/Cuerpo/Ejercicios_modulo.py
@@ -37,10 +37,10 @@ def fun_Vbi(NA,ND,ni,T=300.0):
         - ni = Concetración intrínseca [cm-3]
         - T  = Tempeartura (default 300K)
     """
-    Vbi=(k*T/e)*np.log(NA*ND/(ni**2))
+    Vbi=(k*T)*np.log(NA*ND/(ni**2))
     return Vbi
 
-def fun_xp(NA,ND,KS,Vbieff):
+def fun_xp(NA,ND,KS,Vbi):
     """ 
     Función que calcula xp (anchura en p de la región de vaciamiento) [cm]
         -NA = Numero de Aceptores excitados [cm-3]
@@ -48,7 +48,7 @@ def fun_xp(NA,ND,KS,Vbieff):
         -KS = Constante de 
         -Vbieff = Voltaje de la unión pn en equilibrio (Vbi) o en polarización (Vbi+VA) [eV]
     """
-    xp=(2*KS*cte.epsilon_0/e)*(ND/(NA*(NA+ND)))*(Vbieff*e) 
+    xp=np.sqrt(abs((2*KS*cte.epsilon_0/e)*(ND/(NA*(NA+ND)*10**(6)))*(Vbi)))*10**2
     return xp
 
 def fun_xn(NA,ND,KS,Vbi):
@@ -59,8 +59,8 @@ def fun_xn(NA,ND,KS,Vbi):
         - KS = Constante dieléctrica relativa []
         - Vbi = Voltaje de la unión pn en equilibrio [eV]
     """
-    xp=(2*KS*cte.epsilon_0/e)*(NA/(ND*(NA+ND)))*(Vbi*e) 
-    return xp
+    xn=np.sqrt(abs((2*KS*cte.epsilon_0/e)*(NA/(ND*(NA+ND)*10**(6)))*(Vbi)))*10**2
+    return xn
 
 def fun_Ei(ni,Na,Nd,T=300):
     """ 
@@ -86,11 +86,11 @@ def fun_NCV(mnp,T=300):
 def fun_ni(NC,NV,Eg=EgSi,T=300):
     """ 
     Función que calcula ni (concetración intrínseca) [cm^-3]
-        - NC,NV = [cm-3]
+        - NC,NV = Densidades efectivas N_C y N_V [cm-3]
         - Eg = Energía el gap (por defecto silicio 1.12 eV) [eV]
         - T  = Temperatura (default 300K) [K]
     """
-    ni=np.sqrt(NC*NV)*np.exp(-Eg/(k*T))
+    ni=np.sqrt(NC*NV)*np.exp(-Eg/(2*k*T))
     return ni
 
 
@@ -121,3 +121,70 @@ def fun_mnp(tau,mu):
     """
     mnp=3*e*tau/(mu*10**(-4))
     return mnp
+
+def fun_slope_lineal(NA,ND,KS):
+    """ 
+    Función que calcula la pendiente de V(x) en la región de vaciamiento [V/cm]
+        - KS = permitividad relativa 
+        - NA = Valor de los portadores aceptores [cm-3]. Es 0 si ND!=0
+        - NA = Valor de los portadores dadores [cm-3]. Es 0 si NA!=0
+    """
+    slope=e*NA/(2*KS*cte.epsilon_0*10**(-2))-e*ND/(2*KS*cte.epsilon_0*10**(-2)) 
+    return slope
+
+def fun_grafica_bandas_pn(Ec,Ev,Vbi,Ei,slope_p,slope_n,xn,xp):
+    """
+    Función que grafica las bandas Ec,Ev,Ei,EF a lo largo de una union pn:
+        - Ec: energía de la banda de conducción respecto EF [eV]
+        - Ev: energía de la banda de valecia respecto EF [eV]
+        - Vbi: potencial Vbi [V]
+        - Ei: energía intrínseca respecto EF [eV]
+        - slope_p: pendiente de V(x) en la región de vaciamiento p [V/cm]
+        - slope_n: pendiente de V(x) en la región de vaciamiento n [V/cm]
+        - xn: tamaño de la zona de vaciamiento en n [cm]
+        - xp: tamaño de la zona de vaciamiento en p [cm]        
+    """
+    fig=plt.figure()
+    plt.title("Bandas de Energía")
+    W=xn+xp
+    distancias=np.array([-2*xp,-xp,0,xn,xn+xp])
+    plt.plot([min(distancias),max(distancias)],[0,0],color="black",label="$E_F$")
+    for i in range(len(distancias)-1):
+        if (i==0):
+            plt.plot([distancias[i],distancias[i+1]],[Ec,Ec],color="red",label="$E_C$")
+            plt.plot([distancias[i],distancias[i+1]],[Ev,Ev],color="blue",label="$E_V$")
+            plt.plot([distancias[i],distancias[i+1]],[Ei,Ei],color="green",linestyle="--",label="$E_i$")
+            plt.annotate("", xytext=(-7.2/4*xp, 0), xy=(-7.2/4*xp, Ec),arrowprops=dict(arrowstyle="<->"))
+            plt.annotate("", xytext=(-6.2/4*xp, 0), xy=(-6.2/4*xp, Ei),arrowprops=dict(arrowstyle="<->"))
+            plt.annotate("", xytext=(-6.2/4*xp, 0), xy=(-6.2/4*xp, Ev),arrowprops=dict(arrowstyle="<->"))
+            plt.annotate("$E_C$=%.2f"%Ec, xy=(-7/4*xp, (Ec+Ei)/2))
+            plt.annotate("$E_C$=%.2f"%Ei, xy=(-6/4*xp, Ei/2))
+            plt.annotate("$E_C$=%.2f"%Ev, xy=(-6/4*xp, Ev/2))
+            
+        elif(i==1):
+            x=np.linspace(distancias[i],distancias[i+1],50)   
+            plt.plot(x,-slope_p*(x+xp)**2+Ec,color="red")
+            plt.plot(x,-slope_p*(x+xp)**2+Ev,color="blue")
+            plt.plot(x,-slope_p*(x+xp)**2+Ei,color="green",linestyle="--")
+        elif(i==2):
+            x=np.linspace(distancias[i],distancias[i+1],50)   
+            plt.plot(x,-slope_n*(x-xn)**2+Ec-Vbi,color="red")
+            plt.plot(x,-slope_n*(x-xn)**2+Ev-Vbi,color="blue")
+            plt.plot(x,-slope_n*(x-xn)**2+Ei-Vbi,color="green",linestyle="--")
+        else:         
+            plt.plot([distancias[i],distancias[i+1]],[Ec-Vbi,Ec-Vbi],color="red")
+            plt.plot([distancias[i],distancias[i+1]],[Ev-Vbi,Ev-Vbi],color="blue")
+            plt.plot([distancias[i],distancias[i+1]],[Ei-Vbi,Ei-Vbi],color="green",linestyle="--")
+            plt.annotate("", xytext=(6/8*xp+xn, 0), xy=(6/8*xp+xn, Ec-Vbi),arrowprops=dict(arrowstyle="<->"))
+            plt.annotate("", xytext=(6/8*xp+xn, 0), xy=(6/8*xp+xn, Ei-Vbi),arrowprops=dict(arrowstyle="<->"))
+            plt.annotate("", xytext=(7/8*xp+xn, 0), xy=(7/8*xp+xn, Ev-Vbi),arrowprops=dict(arrowstyle="<->"))
+            plt.annotate("$E_C$=%.2f"%(Ec-Vbi), xy=(0/8*xp+xn, (Ec-Vbi)/2))
+            plt.annotate("$E_C$=%.2f"%(Ei-Vbi), xy=(0/8*xp+xn, (Ei-Vbi)/2))
+            plt.annotate("$E_C$=%.2f"%(Ev-Vbi), xy=(0/8*xp+xn, (Ev+Ei)/2-Vbi))         
+    plt.xlabel("x [cm]")
+    plt.ylabel("E [eV]")
+    plt.grid(True,linestyle="--")
+    plt.legend()
+    
+    return fig
+    
